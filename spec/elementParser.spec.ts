d083208e68991edebe6a73f008ce84f2ab61e255
mkovetsky <mkovetsky@gmail.com>
2019-01-13 11:01:31 +0200

    core(parser): block-2-elements spec added

--- spec/elementParser.spec.ts
+++ spec/elementParser.spec.ts
@@ -9,4 +9,10 @@ describe("element parsing", function (): void {
         const file = loadFile("/beml-examples/element/block-element.beml");
         expect(bemifier.compileSource(file)).toBe('<div class="tabs"><div class="tabs__tab"></div></div>');
     });
+
+    it(" 'Tabs' block and 2 'tab' elements", function (): void {
+        const file = loadFile("/beml-examples/element/block-2-elements.beml");
+        const e = '<div class="tabs"><div class="tabs__tab"></div><div class="tabs__tab"></div></div>';
+        expect(bemifier.compileSource(file)).toBe(e);
+    });
 });
